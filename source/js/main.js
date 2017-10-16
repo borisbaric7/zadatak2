13c3b102a309dab97a40f7c0a89ec53c568ba224
borisbaric7 <borisbaric7@gmail.com>
2017-10-16 22:49:55 +0200

    Validacija forme i slanje POST metodom uz pomoć AJAX-a.

--- source/js/main.js
+++ source/js/main.js
@@ -34,3 +34,60 @@ function showDivs(n) {
   x[slideIndex-1].style.display = "flex";  
   dots[slideIndex-1].className += " w3-white";
 }
+
+//////////////////////////////////////
+// Contact form
+//////////////////////////////////////
+
+function checkName(name){
+  var Text = /^[a-z A-Z]+$/;
+  if(!name.value.match(Text)){
+    name.style.border = "2px solid red";
+    name.value = "";
+  }else{
+    name.style.border = "2px solid green";
+  }
+}
+
+function checkEmail(email){
+  var emailText = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+  if(!email.value.match(emailText)) {
+    email.style.border = "2px solid red";
+  }else{
+    email.style.border = "2px solid green";
+  }
+}
+
+
+$(document).ready(function() {
+
+    $('#contact-form').submit(function(event) {
+
+
+
+        var formData = {
+            'name'              : $('input[name=name]').val(),
+            'email'             : $('input[name=email]').val(),
+            'message'    : $('textarea[name=message]').val()
+        };
+            
+
+        $.ajax({
+            type        : 'POST', 
+            url         : 'www.slicestic.com/api/v1/fe-dev',
+            data        : formData,
+            dataType    : 'json'
+        })
+
+            .done(function() {
+                alert('Poruka je poslana.');
+            })
+            
+            .fail(function() {
+                alert('Došlo je do greške, pokušajte ponovo.');
+            });
+
+        event.preventDefault();
+    });
+
+});
